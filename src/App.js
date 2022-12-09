af78cd0ed44a78da1afd580ca36d71383848dc51
govinduchaitanyakrishna <govinduchaitanya3@gmail.com>
2022-12-09 22:27:58 +0530

    sir

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-    return <div className="App">Hello World</div>;
+    return <div className="App">Hello World Again sir! Trishanth  update</div>;
 }
 
 export default App;
