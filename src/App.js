32230386a25271626640b44a62a3f695509462fa
Capta1nRaj <priyalraj99@gmail.com>
2022-12-09 22:26:52 +0530

    Pushed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-    return <div className="App">Hello World</div>;
+    return (
+        <>
+            <div>Hello Sir.</div>
+            <div>Sir Me Priyal, Me Pushed The Code.</div>
+        </>);
 }
 
 export default App;
