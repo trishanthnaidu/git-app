f64b4f5228bc1a83d69155b689b3eba325f9e7d8
rootzjs <trishnth@gmail.com>
2022-12-10 12:20:56 +0530

    changes for build folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-    return <h1>My First Application Deployment - New !</h1>;
+    return <h1>My First Application Deployment on Netlify</h1>;
 }
 
 export default App;
