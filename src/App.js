56ef7956e5b02f18cd17522c9bc3583d252f6cd0
rootzjs <trishnth@gmail.com>
2022-12-13 23:01:37 +0530

    changes for audio file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import audio from "./horse.ogv";
 
 function App() {
-    return <h1>My First Application Deployment on Vercel</h1>;
+    return (
+        <div>
+            <h1>My First Application Deployment on Vercel</h1>
+            <audio controls>
+                <source src={audio} type="audio/ogg" />
+                Your browser does not support the audio element.
+            </audio>
+        </div>
+    );
 }
 
 export default App;
